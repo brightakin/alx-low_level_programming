0fa2ad57c634cf503eb7746f371f7607fa4a79a3
brightakin <akinkuotubright@gmail.com>
2023-02-21 04:05:08 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - Prints the alphabet 10 times.
+*
+* Return: Always 0
+*/
+void print_alphabet_x10(void)
+{
+	int i;
+
+	char letter;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (letter = "a"; letter <= z; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
