8014ea3d2b15f981cfe8c433d9c1a660d1423a90
brightakin <akinkuotubright@gmail.com>
2023-04-10 10:17:42 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wr = write(fd, text_content, len);
+	wr = write(fd, text_content, ln);
 
 	if (fd == -1 || wr == -1)
 		return (-1);
