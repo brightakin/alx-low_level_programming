70f5222ddc3553ff0189d0edc9c057e5336f10d8
brightakin <akinkuotubright@gmail.com>
2023-04-10 21:14:58 -0700

    0x14-bit_manipulation

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to standard output
+ * @c: The character to print
+ *
+ * Return: On success return 1.
+ * On error, -1 is returned, and error number is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
